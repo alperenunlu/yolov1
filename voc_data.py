dc6dea59eba78747923a59f1179032f9f9c8c844
alperenunlu <97191996+alperenunlu@users.noreply.github.com>
2025-03-09 03:04:36 +0300

    Update normalization parameters in VOCDataModule

--- voc_data.py
+++ voc_data.py
@@ -40,7 +40,7 @@ class VOCDataModule:
                     hue=self.config.Hue,
                 ),
                 v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+                v2.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
                 v2.ClampBoundingBoxes(),
                 v2.SanitizeBoundingBoxes(),
             ]
@@ -53,7 +53,7 @@ class VOCDataModule:
                 v2.ToImage(),
                 v2.Resize(self.config.IMAGE_SIZE),
                 v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+                v2.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
                 v2.ClampBoundingBoxes(),
                 v2.SanitizeBoundingBoxes(),
             ]
